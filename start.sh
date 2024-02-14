e7bade362cd02b24236cf6654cda0a440c3fb06b
OuroborosCaster <yangyanpei20000308@outlook.com>
2024-02-14 22:31:58 +0800

    1.9

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-gunicorn -c gunicorn.conf.py app:app > /dev/stdout 2> /dev/stderr
+# 启动你的应用
+gunicorn -c gunicorn.conf.py app:app &
+
+# 实时查看访问日志文件的内容，并输出到STDOUT
+tail -f logs/access.log &
+
+# 实时查看错误日志文件的内容，并输出到STDERR
+tail -f logs/error.log >&2 &
+
+# 等待所有后台进程
+wait
